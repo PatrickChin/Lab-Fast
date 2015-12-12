0a99eb8229ba713009ca69c1e631e1cb4d9da5b3
Patrick Chin <p.ch@live.co.uk>
2015-12-12 15:25:14 +0000

    No more binary file support

--- muon.c
+++ muon.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
 
 typedef struct muon_measurement {
     unsigned int decay_time;
@@ -48,13 +49,13 @@ mm* parse_ascii(char* c, int *nrows)
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
+    if (argc < 2)
     {
         printf("TODO - useage");
         return -1;
     }
 
-    int fd = open(argv[2], O_RDONLY);
+    int fd = open(argv[1], O_RDONLY);
     if (fd < 0)
     {
         printf("Failed to open \"%s\"\n", argv[1]);
@@ -67,86 +68,36 @@ int main(int argc, char *argv[])
     int nrows;
     mm* data;
 
-    if (argv[1][0] == 'b') // 'b' to read binary file
+    // TODO assume ascii is default
+    // this will change indicies of argv
+    char* strdata = (char*) malloc(fst.st_size);
+    ssize_t read_size = read(fd, strdata, fst.st_size);
+    printf("Read %d bytes of ascii data from \"%s\"\n", read_size, argv[1]);
+
+    if (read_size != fst.st_size+1)
     {
-        // TODO debug why this isn't reading anything
-        ssize_t read_size = read(fd, data, fst.st_size);
-        printf("Read %d bytes of binary data from \"%s\"\n", read_size, argv[2]);
-
-        // This is duplicate code, maybe I should write a function for this
-        if (read_size != fst.st_size)
+        fprintf(stderr, "\nError: Only read %d of the requested %ld.\n", read_size, fst.st_size);
+        char *abort;
+        size_t len = 0;
+        while (1)
         {
-            fprintf(stderr, "\nError: Only read %d of the requested %ld.\n", read_size, fst.st_size);
-            char abort[2] = { 0, 0 };
-            // TODO compare whole string
-            while (abort[0] != 'n')
-            {
-                fprintf(stderr, "Abort? [y/n]: ");
-                scanf("%s", abort);
-                if (abort[0] == 'y')
-                    return 1;
-            }
-        }
-
-        nrows = read_size / sizeof(mm);
-    }
-    else if (argv[1][0] == 'a') // 'a' to read ascii file
-    {
-        // TODO assume ascii is default
-        // this will change indicies of argv
-        char* strdata = (char*) malloc(fst.st_size);
-        ssize_t read_size = read(fd, strdata, fst.st_size);
-        printf("Read %d bytes of ascii data from \"%s\"\n", read_size, argv[2]);
-
-        if (read_size != fst.st_size)
-        {
-            fprintf(stderr, "\nError: Only read %d of the requested %ld.\n", read_size, fst.st_size);
-            char abort[2] = { 0, 0 };
-            // TODO compare whole string
-            while (abort[0] != 'n')
-            {
-                fprintf(stderr, "Abort? [y/n]: ");
-                scanf("%s", abort);
-                if (abort[0] == 'y')
-                    return 1;
-            }
-        }
-
-        data = parse_ascii(strdata, &nrows);
-
-        if (argv[1][1] && argv[1][1] == 'c') // 'c' to create binary file
-        {
-            if (argc < 4) {
-                fprintf(stderr, "\nError: Please enter a binary file to write to as the third argument.\n"
-                        "Continuing without writing a binary file.\n\n");
-                // TODO ask to continue/abort without writing to file
-            } else {
-                FILE *fp = fopen(argv[3], "w+b");
-                if (!fp) {
-                    fprintf(stderr, "Failed to write to \"%s\".\n"
-                            "Continuing without writing a binary file.\n\n", argv[3]);
-                    // TODO ask to continue/abort without writing to file
-                } else {
-                    fwrite(data, sizeof(mm), nrows, fp);
-                }
-                fclose(fp);
-            }
+            fprintf(stderr, "Abort? [y/n]: ");
+            getline(&abort, &len, stdin);
+            if (!strcmp(abort, "yes\x0A") || !strcmp(abort, "y\x0A"))
+                return 1;
+            if (!strcmp(abort, "no\x0A") || !strcmp(abort, "n\x0A"))
+                break;
         }
     }
-    else
-    {
-        fprintf(stderr, "Please specify whether the datafile is ascii 'a' or "
-                "binary 'b' with the first argument of this program.\n");
-        return 1;
-    }
 
+    data = parse_ascii(strdata, &nrows);
 
     printf("nrows: %d\n\n", nrows);
 
     close(fd);
 
     // for (int i = 0; i < 40; i++)
-    //     printf("%3d %6i %10llu\n", i, data[i].decay_time, data[i].time_stamp);
+    //    // printf("%3d %6i %10llu\n", i, data[i].decay_time, data[i].time_stamp);
     // return 0;
 
     int minx = 0, maxx = 15000, binw = 500, nbins = maxx/binw;
@@ -163,11 +114,11 @@ int main(int argc, char *argv[])
     double log_counts[nbins];
     for (int i = 0; i < nbins; ++i)
     {
-        log_counts[i] = counts[i] > 0 ? log((double) counts[i]) : 0;
+        log_counts[i] = log((double) counts[i] + 1);
     }
 
     for (int i = 0, j = binw/2+minx; i < nbins; ++i, j+=binw)
-        printf("%6d %8f\n", j, log_counts[i]);
+        printf("%d %f\n", j, log_counts[i]);
 
     return 0;
 }
