7603db54c8d6244e650f5f89a4ea52bca8abbe62
Patrick Chin <p.ch@live.co.uk>
2016-01-14 17:43:43 +0000

    curve_fit py

--- muon.py
+++ muon.py
@@ -1,8 +1,24 @@
 import sys
 import numpy as np
-import scipy.optimize as opt
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-def loaddata(filename):
-    muondata = np.loadtxt(filename, delimiter=' ', dtype=int)
+muondata = np.loadtxt("data.txt", delimiter=' ', dtype=int)[:,0]
 
+muondata1 = [s for s in muondata if s > 60 and s < 40000]
+hist, edges = np.histogram(muondata1, bins=20)
+mids = (edges[1:] + edges[:-1]) / 2
+loghist = np.log(hist)
 
+def func(x, a, b, c):
+    return np.log(a * np.exp(-x/b) + c)
+
+popt, pvov = curve_fit(func, mids, loghist, (2000, 2200, 6), loghist/2)
+print(popt)
+
+a = np.transpose([mids, hist])
+
+print(a)
+
+plt.plot(mids,loghist,'b.')
+plt.show()
