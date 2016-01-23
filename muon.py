b7e4d403fe1ed309dd8000913f69b30c439317d7
Patrick Chin <p.ch@live.co.uk>
2016-01-23 15:46:20 +0000

    muondata class

--- muon.py
+++ muon.py
@@ -1,5 +1,23 @@
 import numpy as np
 
-dt = np.dtype([('decaytime', '<u4'), ('timestamp', '<u4')])
-data = np.fromfile('datatest.bin', dtype=dt)
+class muondata:
+
+    def __init__(self, path):
+        self.data = np.loadtxt(path, delimiter=" ")
+
+        # if (path.endswith(".txt") or path.endswith(".dat")):
+        #     self.data = np.loadtxt(path, delimiter=" ")
+
+        #     # write to binary file
+        #     if tobin:
+        #         binpath = path[0:-3]+"bin"
+        #         data.tofile(binpath)
+
+        # elif (path.endswith(".bin")):
+        #     dt = np.dtype([('decaytime', '<u4'), ('timestamp', '<u4')])
+        #     self.data = np.fromfile(path, dtype=dt)
+
+        # else:
+        #     print("File should end in '.txt', '.dat' for ascii encoded files"
+        #           "or '.bin' for a binary encoded file")
 
