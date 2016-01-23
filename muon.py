b648b62e72d398ac1322bd150f1ce373ff2811ca
Patrick Chin <p.ch@live.co.uk>
2016-01-23 19:39:19 +0000

    binary support

--- muon.py
+++ muon.py
@@ -1,9 +1,34 @@
+import os
 import numpy as np
+from matplotlib import pyplot as plt
 
 class muondata:
 
-    def __init__(self, path):
-        self.data = np.loadtxt(path, delimiter=" ")
+    def __init__(self, path, binary=False):
+        self.path = path
+
+        dt = np.dtype([('decaytime', np.uint32), ('timestamp', np.uint32)])
+        if binary:
+            self.data = np.fromfile(path, dtype=dt)
+        else:
+            self.data = np.loadtxt(path, delimiter=" ", dtype=dt)
+
+        self.bgdata = self.data[self.data['decaytime'] >= 40000]
+        self.data   = self.data[self.data['decaytime'] < 40000]
+
+        self.analyse()
+
+
+
+    def tobin(self):
+        self.data.tofile(os.path.splitext(self.path)[0]+".bin")
+
+    def analyse(self):
+        self.hist = np.histogram(self.data['decaytime'])
+
+    def plot(self):
+        plt.plot(self.hist.index, self.hist.count)
+
 
         # if (path.endswith(".txt") or path.endswith(".dat")):
         #     self.data = np.loadtxt(path, delimiter=" ")
@@ -21,3 +46,4 @@ class muondata:
         #     print("File should end in '.txt', '.dat' for ascii encoded files"
         #           "or '.bin' for a binary encoded file")
 
+m = muondata('data.bin', True)
