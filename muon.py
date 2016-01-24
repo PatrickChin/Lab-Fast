1f03f8df26e3802d9b63d89fb446eb61970f13f4
Patrick Chin <p.ch@live.co.uk>
2016-01-24 20:32:55 +0000

    histogram options

--- muon.py
+++ muon.py
@@ -16,20 +16,18 @@ class muondata:
         self.bgdata = self.data[self.data['decaytime'] >= 40000]
         self.data   = self.data[self.data['decaytime'] < 40000]
 
-        self.analyse()
-
-
     def tobin(self):
         self.data.tofile(os.path.splitext(self.path)[0]+".bin")
 
-    def analyse(self):
-        self.hist, self.bin_edges = np.histogram(self.data['decaytime'], bins=40)
+    def analyse(self, bins=20, range=None, normed=False, weights=None, density=None):
+        self.hist, self.bin_edges = np.histogram(self.data['decaytime'], bins, range, normed, weights, density)
         self.mids = (self.bin_edges[1:] + self.bin_edges[:-1]) / 2
 
-    def plot(self):
+    def plot(self, logy=True):
         plt.plot(self.mids, self.hist, '+')
         plt.grid(True, which='both')
-        plt.yscale('log')
+        if logy:
+            plt.yscale('log')
         plt.xlabel('Decay time (ns)')
         plt.ylabel('Counts per bin')
         plt.show()
