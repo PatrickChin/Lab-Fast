dc5389460e1a2fcd72efc63ef5538dac14a8467a
Patrick Chin <p.ch@live.co.uk>
2016-01-29 18:46:46 +0000

    created the class

--- low_temp_cal/temp_graph.py
+++ low_temp_cal/temp_graph.py
@@ -5,75 +5,60 @@ from matplotlib.widgets import Slider, Button
 class LowTempGUI():
 
     def __init__(self):
+
+        self.minv = 3
+        self.maxv = 7
+        self.initv = 3
+
+        self.data = []
+        for i in range(self.minv, self.maxv+1):
+            self.data.append(np.loadtxt('data/'+str(i)+'v.txt', delimiter=" "))
+            print('loaded '+str(i)+'v.txt')
+
         self.fig = plt.figure()
         
-        self.fig.add_axes(ax_graph)
-
-        ax_graph = plt.axes([0.1, 0.2, 0.85, 0.7])
-        fig.add_axes(ax_graph)
+        self.ax_graph = plt.axes([0.1, 0.2, 0.85, 0.7])
+        self.fig.add_axes(self.ax_graph)
 
         self.ax_slider = plt.axes([0.1, 0.05, 0.85, 0.04])
-        self.fig.add_axes(ax_slider)
-        self.slider = Slider(ax_slider, 'V', minv, maxv, initv, valfmt='%i')
+        self.fig.add_axes(self.ax_slider)
+        self.slider = Slider(self.ax_slider, 'V', self.minv, self.maxv, self.initv, valfmt='%i')
+        self.slider.on_changed(self.updategraph)
         self.slider.prev_val = -1
+        self.updategraph(self.initv)
 
         self.ax_graph.set_xlabel('Time / s')
         self.ax_graph.set_ylabel('Temperature / Celcius')
         self.ax_graph.set_title('low cal graph test')
 
+        self.connected = False
+        self.fig.canvas.mpl_connect('key_press_event', self.connect_mouse_listener)
+        self.fig.show()
+        plt.show()
 
+    def updategraph(self, val):
+        if self.slider.prev_val == int(val): return
+        self.slider.prev_val = int(val)
+        self.ax_graph.clear()
+        self.ax_graph.plot(self.data[int(val)-self.minv][:,0], self.data[int(val)-self.minv][:,1])
+        self.ax_graph.set_xlim(0, 3000)
+        self.ax_graph.set_ylim(75, 105)
+        self.fig.canvas.draw()
+        
 
-minv = 3
-maxv = 7
-initv = 3
+    def printevt(self, event):
+        if event.xdata != None:
+            print('button={}, x={:.3f}, y={:.3f}'.format(event.button, event.xdata, event.ydata))
 
-dt = np.dtype([('time', np.float), ('temp', np.float)])
-data = []
+    def connect_mouse_listener(self, event):
+        if event.key != ' ': return
+        if self.connected:
+            self.fig.canvas.mpl_disconnect(self.kpe)
+            self.fig.canvas.mpl_disconnect(self.kre)
+        else:
+            self.kpe = self.fig.canvas.mpl_connect('button_press_event', self.printevt)
+            self.kre = self.fig.canvas.mpl_connect('button_release_event', self.printevt)
+        self.connected = not self.connected
 
-for i in range(minv, maxv+1):
-    data.append(np.loadtxt('data/'+str(i)+'v.txt', delimiter=" "))
-    print('loaded '+str(i)+'v.txt')
+LowTempGUI()
 
-fig = plt.figure()
-
-ax_graph = plt.axes([0.1, 0.2, 0.85, 0.7])
-fig.add_axes(ax_graph)
-
-ax_slider = plt.axes([0.1, 0.05, 0.85, 0.04])
-fig.add_axes(ax_slider)
-slider = Slider(ax_slider, 'V', minv, maxv, initv, valfmt='%i')
-slider.prev_val = -1
-
-
-def updategraph(val):
-    if slider.prev_val == int(val): return
-    slider.prev_val = int(val)
-    ax_graph.clear()
-    ax_graph.plot(data[int(val)-minv][:,0], data[int(val)-minv][:,1])
-    ax_graph.set_xlim(0, 2000)
-    ax_graph.set_ylim(75, 105)
-    plt.draw()
-    
-ax_graph.set_xlabel('Time / s')
-ax_graph.set_ylabel('Temperature / Celcius')
-ax_graph.set_title('low cal graph test')
-
-def evnthandler(event):
-    print('button={}, x={:.3f}, y={:.3f}'.format(event.button, event.xdata, event.ydata))
-
-connected = False
-kpe = None
-kre = None
-def connectlistener(e):
-    if connected:
-        fig.canvas.mpl_disconnect(kpe)
-        fig.canvas.mpl_disconnect(kre)
-    else:
-        kpe = fig.canvas.mpl_connect('button_press_event', evnthandler)
-        kre = fig.canvas.mpl_connect('button_release_event', evnthandler)
-
-fig.canvas.mpl_connect('key_press_event', connectlistener)
-
-slider.on_changed(updategraph)
-updategraph(initv)
-plt.show()
