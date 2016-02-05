eaf5ff23f1bf3d5aa399614322f4cc1a519046d4
Patrick Chin <p.ch@live.co.uk>
2016-02-05 20:23:56 +0000

    add multiple selection stages

--- low_temp_cal/lowtempcal.py
+++ low_temp_cal/lowtempcal.py
@@ -7,7 +7,9 @@ dtype = np.dtype([('time',np.int), ('current',np.float),
 def calc_radiated_power(temp, surface_area=0.0010187, emissivity=0.04):
     return surface_area * emissivity * const.Stefan_Boltzmann * (temp**4)
 
+def calc_thermal_conductivity(power, iv, temp_diff, surface_area=5.655e-7, length=0.01):
+    return np.abs(length*(iv-power)/(surface_area*temp_diff))
 
-def calc_k(power, current, voltage, temp_diff, surface_area=5.655e-7, length=0.01):
-    return length*(current*voltage-power)/(surface_area*temp_diff)
+def calc_thermal_conductivity2(power, current, voltage, temp_diff, surface_area=5.655e-7, length=0.01):
+    return np.abs(length*(current*voltage-power)/(surface_area*temp_diff))
 
--- low_temp_cal/span_selector.py
+++ low_temp_cal/span_selector.py
@@ -5,10 +5,15 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import SpanSelector
 import lowtempcal
 
+plt.style.use('bmh')
+
+filename = './7V.csv'
+# filename = raw_input("File name: ")
 
 measurement_interval = 0.5
+# measurement_interval = np.float(raw_input("Time interval: "))
 
-data = np.loadtxt('./rawdata/7V.csv', delimiter=',', dtype=lowtempcal.dtype)
+data = np.loadtxt(filename, delimiter=',', dtype=lowtempcal.dtype)
 data = data[::-1]
 data['time'] = data['time']*measurement_interval
 
@@ -21,9 +26,12 @@ fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot(211, axisbg='#FFFFCC')
 
 ax.plot(time, temp, '-')
-ax.set_title('Press left mouse button and drag to test')
+ax.set_title('Press left mouse button and drag to zoom')
+txt = ax.text(100,(max(temp)+min(temp))/2.0,'$T_{{\mathrm{{base}}}} = ?$\n'
+                                            '$T_{{\mathrm{{max}}}} = ?$', size='xx-large')
 
 ax2 = fig.add_subplot(212, axisbg='#FFFFCC')
+ax2.set_title('Select area to to calculate base temperature')
 line2, = ax2.plot(time, temp, '-')
 
 
@@ -38,33 +46,80 @@ def onselecttop(xmin, xmax):
     ax2.set_ylim(thisy.min(), thisy.max())
     fig.canvas.draw()
 
+stage = 0
+blockenter = True
+TBASE = 0
+TMAX = 1
+CV = 2
+
+def calc_means(start,end):
+    t = np.mean(np.array(temp[start:end]), dtype=np.float64)
+    tstd  = np.std(np.array(temp[start:end]))
+    i = np.mean(np.array(current[start:end]))
+    v = np.mean(np.array(volt[start:end]))
+    iv = np.mean(np.array(volt[start:end])*np.array(volt[start:end]))
+    return t, tstd, i, v, iv
 
 def onselectbot(xmin, xmax):
+    global stage, blockenter
+    global tempbase, tempbasestd, tempbasei, tempbasev
+    global tempmax, tempmaxstd, tempmaxi, tempmaxv
+
     indmin, indmax = np.searchsorted(time, (xmin, xmax))
     indmax = min(len(time)-1, indmax)
     indmin = max(0, indmin)
-    
-    np.set_printoptions(precision=4)
-    thisy = np.array(temp[indmin:indmax])
-    ymean = np.mean(thisy, dtype=np.float64)
-    ystd  = np.std(thisy)
-    print("Temperature: {} ({})".format(ymean, ystd))
 
-    p = lowtempcal.calc_radiated_power(ymean)
-    print("Power: {}".format(p))
+    blockenter = False
+    if stage == TBASE:
+        tempbase, tempbasestd, tempbasei, tempbasev, tempbaseiv = calc_means(indmin,indmax)
+        print("Temperature (base): {:.2f} ({:.2f})".format(tempbase, tempbasestd))
+        print("(If that selection of the base temperature was wrong press enter to reselect)\n")
+        txt.set_text("$T_{{\mathrm{{base}}}} = {:.2f} ({:.2f})$\n"
+                     "$T_{{\mathrm{{max}}}} =  ?$".format(tempbase, tempbasestd))
+        ax2.set_title('Select area to to calculate max temperature and thermal conductivity')
 
-    i = np.mean(np.array(current[indmin:indmax]))
-    v = np.mean(np.array(volt[indmin:indmax]))
-    k = lowtempcal.calc_k(p, i, v, max(thisy)-min(thisy))
-    print("Thermal Conductivity: {}\n".format(k))
+    elif stage == TMAX:
+        tempmax, tempmaxstd, tempmaxi, tempmaxv, tempmaxiv = calc_means(indmin,indmax)
+        p = lowtempcal.calc_radiated_power(tempmax)
+        k = lowtempcal.calc_thermal_conductivity(p, tempmaxiv, tempmax-tempbase)
+        print("Temperature (max): {:.2f} ({:.2f})".format(tempmax, tempmaxstd))
+        print("(If that selection of the max temperature was wrong press enter to reselect)\n")
+
+        print("Temperature (base): {:.2f} ({:.2f})".format(tempbase, tempbasestd))
+        print("Temperature (max): {:.2f} ({:.2f})".format(tempmax, tempmaxstd))
+        print("Power: {:.2g}".format(p))
+        print("Thermal Conductivity: {:.2f}\n\n".format(k))
+        txt.set_text("$T_{{\mathrm{{base}}}} = {:.2f} ({:.2f})$\n"
+                     "$T_{{\mathrm{{max}}}} = {:.2f} ({:.2f})$"
+                     .format(tempbase, tempbasestd, tempmax, tempmaxstd))
+        ax2.set_title('Select area to to calculate base temperature')
+
+    fig.canvas.draw()
+    stage += 1
+    stage %= 2
 
 
+def onkeypress(event):
+    global stage, blockenter, ax2
+    if event.key == u'enter' and not blockenter:
+        blockenter = True
+        if stage == TMAX:
+            stage = TBASE
+            ax2.set_title('Select area to to calculate base temperature')
+            txt.set_text("$T_{{\mathrm{{base}}}} =  ?$\n"
+                         "$T_{{\mathrm{{max}}}} =  ?$")
+        elif stage == TBASE:
+            stage = TMAX
+            txt.set_text("$T_{{\mathrm{{base}}}} = {:.2f} ({:.2f})$\n"
+                         "$T_{{\mathrm{{max}}}} =  ?$".format(tempbase, tempbasestd))
+            ax2.set_title('Select area to to calculate max temperature and thermal conductivity')
 
-span = SpanSelector(ax, onselecttop, 'horizontal', useblit=False,
-                    rectprops=dict(alpha=0.5, facecolor='red') )
-span2 = SpanSelector(ax2, onselectbot, 'horizontal', useblit=False,
-                    rectprops=dict(alpha=0.1, facecolor='red') )
+        fig.canvas.draw()
 
+
+span = SpanSelector(ax, onselecttop, 'horizontal', useblit=False, rectprops=dict(alpha=0.5, facecolor='black') )
+span2 = SpanSelector(ax2, onselectbot, 'horizontal', useblit=False, rectprops=dict(alpha=0.3, facecolor='black') )
+cid = fig.canvas.mpl_connect('key_release_event', onkeypress)
 
 plt.show()
 
