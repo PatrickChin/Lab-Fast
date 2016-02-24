05a29958d3c4d2506d6c9d57f194dcd158624bd7
Patrick Chin <p.ch@live.co.uk>
2016-02-24 22:39:37 +0000

    organised things

--- low_temp_cal/lowtempcal.py
+++ low_temp_cal/lowtempcal.py
@@ -1,8 +1,23 @@
+import os
 import numpy as np
 import scipy.constants as const
+from PyQt5 import QtCore, QtWidgets
 
 dtype = np.dtype([('time',np.int), ('current',np.float),
     ('voltage',np.float), ('temperature',np.float)])
+
+class LowTempCalItem(QtWidgets.QListWidgetItem):
+
+    def __init__(self, string, data, time_interval=1.0, parent=None):
+        QtWidgets.QListWidgetItem.__init__(self, string, parent)
+        self.data = data
+        self.time_interval = time_interval
+
+    @staticmethod
+    def from_file(filename, parent=None):
+        data = np.loadtxt(filename, delimiter=',', dtype=dtype)
+        data = data[::-1]
+        return LowTempCalItem(os.path.basename(filename), data, parent)
 
 def calc_radiated_power(temp, surface_area=0.0010187, emissivity=0.04):
     return surface_area * emissivity * const.Stefan_Boltzmann * (temp**4)
