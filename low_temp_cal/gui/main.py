a9e86d1edba386411ece5e9961c0487cc6198b6a
Patrick Chin <p.ch@live.co.uk>
2016-02-24 22:13:37 +0000

    import data with file dialog

--- low_temp_cal/gui/main.py
+++ low_temp_cal/gui/main.py
@@ -1,9 +1,11 @@
+import os
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.uic import loadUiType
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
+from lowtempcal import LowTempCalItem
 
 Ui_MainWindow, QMainWindow = loadUiType('mainwindow.ui')
 
@@ -12,26 +14,34 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         super(Main, self).__init__()
         self.setupUi(self)
 
-        self.file_import_button.clicked.connect(self.importButton)
-
-    def importButton(self):
-        filename = QtWidgets.QFileDialog.getOpenFileName(self)
-        self.file_list.addItem(filename)
-
-    def addmpl(self, fig):
-        self.canvas = FigureCanvas(fig)
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+        self.canvas = FigureCanvas(self.fig)
         self.graph_layout.addWidget(self.canvas)
         self.canvas.draw()
  
+        self.prev_dir = '.'
+        self.file_import_button.clicked.connect(self.importButton)
+        self.files = []
+
+    def importButton(self):
+        filenames = QtWidgets.QFileDialog.getOpenFileNames(
+                self, 'Select one or more data files to import', directory=self.prev_dir)
+        self.prev_dir = os.path.dirname(filenames[0][0])
+        for f in filenames[0]: # unsure why this is an array of array
+            if f in self.files:
+                print('Not importing \"{}\", it was already imported'.format(f))
+                break
+            self.files.append(f)
+            cur_item = LowTempCalItem.from_file(f, self.file_list)
+            self.file_list.addItem(cur_item)
+            self.ax.plot(cur_item.data['time'], cur_item.data['temperature'])
+            self.canvas.draw()
+
 if __name__ == '__main__':
     import sys
-    import numpy as np
- 
-    fig1 = Figure()
-    ax1 = fig1.add_subplot(111)
  
     app = QtWidgets.QApplication(sys.argv)
     main = Main()
-    main.addmpl(fig1)
     main.show()
     sys.exit(app.exec_())
