7d1ef0d8ac51e3b0a23cf98dc6613c4845c5fe91
Patrick Chin <p.ch@live.co.uk>
2016-03-06 16:42:46 +0000

    clean LowTempCalData class

--- lowtempcal/lowtempcal.py
+++ lowtempcal/lowtempcal.py
@@ -15,86 +15,8 @@ from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 
-class LowTempCalData:
 
-    dtype = np.dtype([('time',np.int), ('current',np.float),
-        ('voltage',np.float), ('temperature',np.float)])
 
-    def __init__(self, filename):
-        self.voltage = 1
-        self.length = 1
-        self.area = 1
-        self.dt = 1
-
-        self.tmin_x1 = 1
-        self.tmin_x2 = 1
-        self.tmin = 1
-        self.tmin_std = 1
-
-        self.tmax_x1 = 1
-        self.tmax_x2 = 1
-        self.tmax = 1
-        self.tmax_std = 1
-
-        self.data = np.loadtxt(filename, delimiter=',', dtype=LowTempCalData.dtype)[::-1]
-        self.line = Line2D(self.data['time'], self.data['temperature'], label=os.path.basename(filename), visible=False)
-        self.line_current = Line2D(self.data['time'], self.data['current'], label=os.path.basename(filename), visible=False, color="red")
-
-Ui_MainWindow, QMainWindow = loadUiType('mainwindow.ui')
-
-class LowTempCalValueGroup(QtWidgets.QWidget):
-
-    def __init__(self, text, parent):
-        # QtWidgets.QWidget.__init__(parent=parent)
-        super().__init__()
-
-        self.layout = QtWidgets.QFormLayout(self)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setSpacing(6)
-
-        self.radio = QtWidgets.QRadioButton(text)
-        self.radio.toggled.connect(self.setEnabled)
-
-        self.label_start = QtWidgets.QLabel("Start", parent=self)
-        self.label_start.setIndent(22)
-        self.layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_start)
-
-        self.start = QtWidgets.QSpinBox(self)
-        self.layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.start)
-
-        self.label_end = QtWidgets.QLabel("End", parent=self)
-        self.label_end.setIndent(22)
-        self.layout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_end)
-
-        self.end = QtWidgets.QSpinBox(self)
-        self.end.setButtonSymbols(QtWidgets.QAbstractSpinBox.UpDownArrows)
-        self.layout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.end)
-
-        self.labelue = QtWidgets.QLabel("Value", parent=self)
-        self.labelue.setIndent(22)
-        self.layout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.labelue)
-
-        self.value = QtWidgets.QDoubleSpinBox(self)
-        self.value.setReadOnly(True)
-        self.value.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
-        self.layout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.value)
-
-        self.label_std = QtWidgets.QLabel("Std", parent=self)
-        self.label_std.setIndent(22)
-        self.layout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_std)
-
-        self.std = QtWidgets.QDoubleSpinBox(self)
-        self.std.setReadOnly(True)
-        self.std.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
-        self.layout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.std)
-
-        self.setEnabled(False)
-
-    def setues(self, x1, x2, val, std):
-        self.start.setValue(x1)
-        self.end.setValue(x2)
-        self.value.setValue(val)
-        self.std.setValue(std)
 
 def calc_radiated_power(temp, surface_area=1.0187e-3, emissivity=4e-2):
     return surface_area * emissivity * const.Stefan_Boltzmann * (temp**4)
@@ -106,6 +28,102 @@ def calc_thermal_conductivity2(power, current, voltage, temp_diff, surface_area=
     return np.abs(length*(current*voltage-power)/(surface_area*temp_diff))
 
 
+
+
+
+class LowTempCalData:
+
+    dtype = np.dtype([('time',np.int), ('current',np.float),
+        ('voltage',np.float), ('temperature',np.float)])
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.basename = os.path.basename(filename)
+        self.voltage = 5
+        self.length = 0.01
+        self.area = 1.0187e-3
+        self.dt = 1
+
+        self.power = 0
+        self.kt = 0
+
+        self.tmin_x1 = 0
+        self.tmin_x2 = 0
+        self.tmin = 0
+        self.tmin_std = 0
+
+        self.tmax_x1 = 0
+        self.tmax_x2 = 0
+        self.tmax = 0
+        self.tmax_std = 0
+
+        data = np.loadtxt(filename, delimiter=',', dtype=LowTempCalData.dtype)[::-1]
+        self.time = data['time']
+        self.temp = data['temperature']
+        self.current = data['current']
+        self.line = Line2D(self.time, self.temp, label=self.basename, visible=False)
+        self.line_current = Line2D(self.time, self.current, label=self.basename, visible=False, color="red")
+
+
+
+
+
+class LowTempCalValueGroup(QtWidgets.QWidget):
+
+    def __init__(self, text, parent):
+        super().__init__()
+
+        # Add form layout to widget
+        self.layout = QtWidgets.QFormLayout(self)
+
+        # Radio button is added to parent
+        self.radio = QtWidgets.QRadioButton(text)
+        self.radio.toggled.connect(self.setEnabled)
+        parent.addRow(self.radio)
+
+        self.label_start = QtWidgets.QLabel("Start", parent=self)
+        self.label_start.setIndent(22)
+        self.layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_start)
+        self.start = QtWidgets.QSpinBox(self)
+        self.layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.start)
+
+        self.label_end = QtWidgets.QLabel("End", parent=self)
+        self.label_end.setIndent(22)
+        self.layout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_end)
+        self.end = QtWidgets.QSpinBox(self)
+        self.end.setButtonSymbols(QtWidgets.QAbstractSpinBox.UpDownArrows)
+        self.layout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.end)
+
+        self.labelue = QtWidgets.QLabel("Value", parent=self)
+        self.labelue.setIndent(22)
+        self.layout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.labelue)
+        self.value = QtWidgets.QDoubleSpinBox(self)
+        self.value.setReadOnly(True)
+        self.value.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.layout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.value)
+
+        self.label_std = QtWidgets.QLabel("Std", parent=self)
+        self.label_std.setIndent(22)
+        self.layout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_std)
+        self.std = QtWidgets.QDoubleSpinBox(self)
+        self.std.setReadOnly(True)
+        self.std.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.layout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.std)
+
+        self.setEnabled(False)
+
+        parent.addRow(self)
+
+    def setues(self, x1, x2, val, std):
+        self.start.setValue(x1)
+        self.end.setValue(x2)
+        self.value.setValue(val)
+        self.std.setValue(std)
+
+
+
+
+Ui_MainWindow, QMainWindow = loadUiType('mainwindow.ui')
 class LowTempCalApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self):
@@ -119,46 +137,47 @@ class LowTempCalApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.graph_layout.addWidget(self.canvas)
         self.canvas.draw()
 
-        self.checkbox_show_current.stateChanged.connect(self.show_current_graph)
-
+        # Show matplotlib toolbar
         self.toolbar = NavigationToolbar(self.canvas, self, coordinates=True)
         self.graph_layout.addWidget(self.toolbar)
  
-        self.prev_dir = '.'
+        self.checkbox_show_current.stateChanged.connect(self.show_current_graph)
+
+        # Import file button
         self.file_import_button.clicked.connect(self.import_dialog)
         self.filenames = []
         self.data = []
         self.cur_index = 0
 
+        # file selector list
         self.file_list_box.currentIndexChanged.connect(self.file_change)
 
+        # Input boxes for tmin and tmax
+        self.tmin_group = LowTempCalValueGroup("Min Temperature", self.value_layout)
+
+        # Input boxes for tmax
         self.tmax_group = LowTempCalValueGroup("Max Temperature", self.value_layout)
         self.value_layout.addRow(self.tmax_group.radio)
         self.value_layout.addRow(self.tmax_group)
 
-        self.tmin_group = LowTempCalValueGroup("Min Temperature", self.value_layout)
-        self.value_layout.addRow(self.tmin_group.radio)
-        self.value_layout.addRow(self.tmin_group)
-
-        self.tmax_group.radio.toggle()
+        # select tmin automatically
+        self.tmin_group.radio.toggle()
 
         self.span = SpanSelector(self.ax_temp, self.on_span_select, 'horizontal', useblit=False,
                 span_stays=False, rectprops=dict(alpha=0.1, facecolor='blue') )
 
     def show_current_graph(self, state):
-        print("state: {}".format(state))
-        if state:
-            self.ax_current.set_axis_off()
-        else:
-            self.ax_current.set_axis_on()
+        self.data[self.cur_index].line_current.set_visible(state)
+        self.ax_current.get_yaxis().set_visible(state)
+        self.canvas.draw()
 
 
     def on_span_select(self, x1, x2):
         d = self.data[self.cur_index]
         x1 = max(0, int(x1/d.dt))
-        x2 = min(len(d.data)-1, int(x2/d.dt))
-        temp = d.data['temperature'][x1:x2]
-        time = d.data['time'][x1:x2]
+        x2 = min(len(d.time)-1, int(x2/d.dt))
+        temp = d.temp[x1:x2]
+        time = d.time[x1:x2]
         if self.tmax_group.radio.isChecked():
             val = np.mean(temp)
             std = np.std(temp)
@@ -180,7 +199,7 @@ class LowTempCalApp(QtWidgets.QMainWindow, Ui_MainWindow):
         dtemp = d.tmax - d.tmin 
         if dtemp > 0.1:
             power = calc_radiated_power(d.tmax) # , self.spinbox_area.value())
-            current = np.mean(d.data['current'][d.tmax_x1:d.tmax_x2])
+            current = np.mean(d.current[d.tmax_x1:d.tmax_x2])
             d.voltage = self.spinbox_voltage.value()
             d.kt = calc_thermal_conductivity(power, current*d.voltage, dtemp) #, self.spinbox_area.value(), self.spinbox_len.value())
             self.spinbox_kt.setValue(d.kt)
@@ -202,8 +221,14 @@ class LowTempCalApp(QtWidgets.QMainWindow, Ui_MainWindow):
         d = self.data[index]
         self.tmax_group.setues(d.tmax_x1, d.tmax_x2, d.tmax, d.tmax_std)
         self.tmin_group.setues(d.tmin_x1, d.tmin_x2, d.tmin, d.tmin_std)
+        self.spinbox_dt.setValue(d.dt)
+        self.spinbox_voltage.setValue(d.voltage)
+        self.spinbox_length.setValue(d.length)
+        self.spinbox_area.setValue(d.area)
+        self.spinbox_power.setValue(d.power)
+        self.spinbox_kt.setValue(d.kt)
 
-        ld = len(d.data)
+        ld = len(d.time)
         self.tmax_group.start.setMaximum(ld)
         self.tmin_group.start.setMaximum(ld)
         self.tmax_group.end.setMaximum(ld)
@@ -211,7 +236,7 @@ class LowTempCalApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def import_dialog(self):
         filenames, _ = QtWidgets.QFileDialog.getOpenFileNames(
-                self, 'Select one or more data files to import', directory=self.prev_dir)
+                self, 'Select one or more data files to import') #, directory=self.prev_dir)
         if len(filenames) < 1:
             return
         self.import_files(filenames)
