1edb0a9979292867634f48c63526fe6d3f087006
Patrick Chin <p.ch@live.co.uk>
2016-03-07 23:10:45 +0000

    added ascii to binary script

---
+++ lowtempcal/tobin.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+
+dtype = np.dtype([('time',np.int), ('current',np.float),
+    ('voltage',np.float), ('temperature',np.float)])
+
+os.mkdir("binary_data")
+for f in os.listdir("./data"):
+    data = np.loadtxt("./data/"+f, delimiter=',', dtype=dtype)[::-1]
+    f = os.path.basename(f)
+    f = os.path.splitext(f)[0]
+    data.tofile("./binary_data/"+f+".bin")
